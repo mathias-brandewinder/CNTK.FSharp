81662460bb8da844d36948a6ff1e9ee07b691f28
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2017-12-27 18:07:27 -0800

    WIP: Sequential models

---
+++ CNTK.Sequential.fsx
@@ -0,0 +1,318 @@
+(*
+This file is intended to load dependencies in an F# script,
+to train a model from the scripting environment.
+CNTK, CPU only, is assumed to have been installed via Paket.
+*)
+
+open System
+open System.IO
+open System.Collections.Generic
+
+Environment.SetEnvironmentVariable("Path",
+    Environment.GetEnvironmentVariable("Path") + ";" + __SOURCE_DIRECTORY__)
+
+let dependencies = [
+        "./packages/CNTK.CPUOnly/lib/net45/x64/"
+        "./packages/CNTK.CPUOnly/support/x64/Dependency/"
+        "./packages/CNTK.CPUOnly/support/x64/Dependency/Release/"
+        "./packages/CNTK.CPUOnly/support/x64/Release/"    
+    ]
+
+dependencies 
+|> Seq.iter (fun dep -> 
+    let path = Path.Combine(__SOURCE_DIRECTORY__,dep)
+    Environment.SetEnvironmentVariable("Path",
+        Environment.GetEnvironmentVariable("Path") + ";" + path)
+    )    
+
+#I "./packages/CNTK.CPUOnly/lib/net45/x64/"
+#I "./packages/CNTK.CPUOnly/support/x64/Dependency/"
+#I "./packages/CNTK.CPUOnly/support/x64/Dependency/Release/"
+#I "./packages/CNTK.CPUOnly/support/x64/Release/"
+
+#r "./packages/CNTK.CPUOnly/lib/net45/x64/Cntk.Core.Managed-2.3.1.dll"
+open CNTK
+
+// utilities
+
+let shape (dims:int seq) = NDShape.CreateNDShape dims
+
+let isSweepEnd (minibatchValues: seq<MinibatchData>) =
+    minibatchValues 
+    |> Seq.exists(fun a -> a.sweepEnd)
+
+// Sequential model
+
+type Computation = DeviceDescriptor -> Variable -> Function
+
+type Loss = 
+    | CrossEntropyWithSoftmax
+    | ClassificationError
+
+let evaluation (loss:Loss) (predicted:Function, actual:Variable) =
+    match loss with
+    | CrossEntropyWithSoftmax -> 
+        CNTKLib.CrossEntropyWithSoftmax(new Variable(predicted),actual)
+    | ClassificationError -> 
+        CNTKLib.ClassificationError(new Variable(predicted),actual)
+
+type Schedule = {
+    Rate: float
+    MinibatchSize: int
+    }
+
+type Specification = {
+    Features: Variable
+    Labels: Variable
+    Model: Computation
+    Loss: Loss
+    Eval: Loss
+    Schedule: Schedule
+    }
+
+type Config = {
+    MinibatchSize: int
+    Epochs: int
+    Device: DeviceDescriptor
+    }    
+
+let learning (predictor:Function) (schedule:Schedule) =   
+    let learningRatePerSample = 
+        new TrainingParameterScheduleDouble(schedule.Rate, uint32 schedule.MinibatchSize)
+    let parameterLearners =
+        ResizeArray<Learner>(
+            [ 
+                Learner.SGDLearner(predictor.Parameters(), learningRatePerSample) 
+            ])
+    parameterLearners
+
+let prepare (device:DeviceDescriptor) (spec:Specification) =
+    
+    let predictor = spec.Model device spec.Features
+    let loss = evaluation spec.Loss (predictor,spec.Labels)
+    let eval = evaluation spec.Eval (predictor,spec.Labels)   
+    let parameterLearners = learning predictor spec.Schedule     
+    let trainer = Trainer.CreateTrainer(predictor, loss, eval, parameterLearners)
+
+    predictor, trainer
+
+let learn 
+    (source:MinibatchSource) 
+    (featureStreamName:string, labelsStreamName:string) 
+    (config:Config) 
+    (spec:Specification) =
+
+    let device = config.Device
+    let (predictor,trainer) = prepare device spec    
+    let input = spec.Features
+    let labels = spec.Labels
+    let featureStreamInfo = source.StreamInfo(featureStreamName)
+    let labelStreamInfo = source.StreamInfo(labelsStreamName)
+    let minibatchSize = uint32 (config.MinibatchSize)
+
+    let rec learnEpoch (step,epoch) = 
+
+        if epoch <= 0
+        // we are done : return function
+        then predictor
+        else
+            let step = step + 1
+            let minibatchData = source.GetNextMinibatch(minibatchSize, device)
+
+            let arguments : IDictionary<Variable, MinibatchData> =
+                [
+                    input, minibatchData.[featureStreamInfo]
+                    labels, minibatchData.[labelStreamInfo]
+                ]
+                |> dict
+
+            trainer.TrainMinibatch(arguments, device) |> ignore
+            
+            // MinibatchSource is created with MinibatchSource.InfinitelyRepeat.
+            // Batching will not end. Each time minibatchSource completes an sweep (epoch),
+            // the last minibatch data will be marked as end of a sweep. We use this flag
+            // to count number of epochs.
+            let epoch = 
+                if isSweepEnd (minibatchData.Values)
+                then epoch - 1
+                else epoch
+
+            learnEpoch (step, epoch)
+
+    learnEpoch (0, config.Epochs)
+
+[<RequireQualifiedAccess>]
+module Layer = 
+    
+    // Combine 2 Computation Layers into 1
+    let stack (next:Computation) (curr:Computation) : Computation =
+        fun device ->
+            fun variable ->
+                let intermediate = new Variable(curr device variable)
+                next device intermediate
+
+    let scale<'T> (scalar:'T) : Computation = 
+        fun device ->
+            fun input ->
+                CNTKLib.ElementTimes(Constant.Scalar<'T>(scalar, device), input)
+
+    let dense (outputDim:int) : Computation =
+        fun device ->
+            fun input ->
+               
+                let input : Variable =
+                    if (input.Shape.Rank <> 1)
+                    then
+                        let newDim = input.Shape.Dimensions |> Seq.reduce (*)
+                        new Variable(CNTKLib.Reshape(input, shape [ newDim ]))
+                    else input
+
+                let inputDim = input.Shape.[0]
+                let dataType = input.DataType
+
+                let weights = 
+                    new Parameter(
+                        shape [outputDim; inputDim], 
+                        dataType,
+                        CNTKLib.GlorotUniformInitializer(
+                            float CNTKLib.DefaultParamInitScale,
+                            CNTKLib.SentinelValueForInferParamInitRank,
+                            CNTKLib.SentinelValueForInferParamInitRank, 
+                            uint32 1),
+                        device, 
+                        "weights")
+
+                let product = 
+                    new Variable(CNTKLib.Times(weights, input, "product"))
+
+                let bias = new Parameter(shape [ outputDim ], 0.0f, device, "bias")
+                CNTKLib.Plus(bias, product)
+
+    let dropout (proba:float) : Computation = 
+        fun device ->
+            fun input ->
+                CNTKLib.Dropout(input,proba)
+
+[<RequireQualifiedAccess>]
+module Activation = 
+
+    let ReLU : Computation = 
+        fun device ->
+            fun input ->
+                CNTKLib.ReLU(input)
+
+    let sigmoid : Computation = 
+        fun device ->
+            fun input ->
+                CNTKLib.Sigmoid(input)
+
+    let tanh : Computation = 
+        fun device ->
+            fun input ->
+                CNTKLib.Tanh(input)
+
+
+[<RequireQualifiedAccess>]
+module Conv2D = 
+    
+    type Kernel = {
+        Width: int
+        Height: int
+        }
+
+    type Conv2D = {
+        Kernel: Kernel 
+        InputChannels: int
+        OutputFeatures: int
+        }
+    let convolution (args:Conv2D) : Computation = 
+        fun device ->
+            fun input ->
+                let convWScale = 0.26
+                let kernel = args.Kernel
+                let convParams = 
+                    new Parameter(
+                        shape [ kernel.Width; kernel.Height; args.InputChannels; args.OutputFeatures ], 
+                        DataType.Float,
+                        CNTKLib.GlorotUniformInitializer(convWScale, -1, 2), 
+                        device)
+
+                CNTKLib.Convolution(
+                    convParams, 
+                    input, 
+                    shape [ 1; 1; args.InputChannels ]
+                    )
+
+    type Window = {
+        Width: int
+        Height: int          
+        }
+
+    type Stride = {
+        Horizontal: int
+        Vertical: int
+        }
+
+    type Pool2D = {
+        Window: Window
+        Stride : Stride 
+        PoolingType : PoolingType
+        }                
+    let pooling (args:Pool2D) : Computation = 
+        fun device ->
+            fun input ->
+
+                let window = args.Window
+                let stride = args.Stride
+
+                CNTKLib.Pooling(
+                    input, 
+                    args.PoolingType,
+                    shape [ window.Width; window.Height ], 
+                    shape [ stride.Horizontal; stride.Vertical ], 
+                    [| true |]
+                    )
+let private dictAdd<'K,'V> (key,value) (dict:Dictionary<'K,'V>) = 
+    dict.Add(key,value)
+    dict
+
+let dataMap xs = 
+    let dict = Dictionary<Variable,Value>()
+    xs |> Seq.fold (fun dict (var,value) -> dictAdd (var,value) dict) dict
+
+type TrainingMiniBatchSummary = {
+    Loss:float
+    Evaluation:float
+    Samples:uint32
+    TotalSamples:uint32
+    }
+
+let minibatchSummary (trainer:Trainer) =
+    if trainer.PreviousMinibatchSampleCount () <> (uint32 0)
+    then
+        {
+            Loss = trainer.PreviousMinibatchLossAverage ()
+            Evaluation = trainer.PreviousMinibatchEvaluationAverage ()
+            Samples = trainer.PreviousMinibatchSampleCount ()
+            TotalSamples = trainer.TotalNumberOfSamplesSeen ()
+        }
+    else
+        {
+            Loss = Double.NaN
+            Evaluation = Double.NaN
+            Samples = trainer.PreviousMinibatchSampleCount ()
+            TotalSamples = trainer.TotalNumberOfSamplesSeen ()
+        }
+
+let progress (trainer:Trainer, frequency) current =
+    if current % frequency = 0
+    then Some(current, minibatchSummary trainer)
+    else Option.None
+
+let printer = 
+    Option.iter (fun (batch,report) ->
+        printf "Batch: %i " batch
+        printf "Average Loss: %.3f " (report.Loss)
+        printf "Average Eval: %.3f " (report.Evaluation)
+        printfn ""
+        )
---
+++ examples/MNIST-CNN.Seq.fsx
@@ -0,0 +1,193 @@
+(*
+F# port of the original C# example from the CNTK docs:
+https://github.com/Microsoft/CNTK/blob/master/Examples/TrainingCSharp/Common/MNISTClassifier.cs
+*)
+
+// Use the CNTK.fsx file to load the dependencies.
+
+#load "../CNTK.Sequential.fsx"
+open CNTK
+open CNTK.Sequential
+open System
+open System.IO
+open System.Collections.Generic
+
+// definition / configuration of the network
+
+let imageSize = 28 * 28
+let numClasses = 10
+let input = CNTKLib.InputVariable(shape [ 28; 28; 1 ], DataType.Float)
+let labels = CNTKLib.InputVariable(shape [ numClasses ], DataType.Float)
+
+let network : Computation =
+    Layer.scale (float32 (1./255.))
+    |> Layer.stack (Conv2D.convolution 
+        {    
+            Kernel ={ Width = 3; Height = 3 } 
+            InputChannels = 1
+            OutputFeatures = 4
+        }
+        )
+    |> Layer.stack Activation.ReLU
+    |> Layer.stack (Conv2D.pooling
+        {
+            PoolingType = PoolingType.Max
+            Window = { Width = 3; Height = 3 }
+            Stride = { Horizontal = 2; Vertical = 2 }
+        }
+        )
+    |> Layer.stack (Conv2D.convolution
+        {    
+            Kernel ={ Width = 3; Height = 3 } 
+            InputChannels = 4 // matches previous conv output
+            OutputFeatures = 8
+        }
+        )
+    |> Layer.stack Activation.ReLU
+    |> Layer.stack (Conv2D.pooling
+        {
+            PoolingType = PoolingType.Max
+            Window = { Width = 3; Height = 3 }
+            Stride = { Horizontal = 2; Vertical = 2 }
+        }
+        )
+    |> Layer.stack (Layer.dense numClasses)
+
+let spec = {
+    Features = input
+    Labels = labels
+    Model = network
+    Loss = CrossEntropyWithSoftmax
+    Eval = ClassificationError
+    Schedule = { Rate = 0.003125; MinibatchSize = 1 }
+    }
+
+// learning
+let ImageDataFolder = Path.Combine(__SOURCE_DIRECTORY__, "../data/")
+let featureStreamName = "features"
+let labelsStreamName = "labels"
+
+let streamConfigurations = 
+    ResizeArray<StreamConfiguration>(
+        [
+            new StreamConfiguration(featureStreamName, imageSize)    
+            new StreamConfiguration(labelsStreamName, numClasses)
+        ]
+        )
+
+let modelFile = Path.Combine(__SOURCE_DIRECTORY__,"MNISTConvolution.model")
+
+let minibatchSource = 
+    MinibatchSource.TextFormatMinibatchSource(
+        Path.Combine(ImageDataFolder, "Train_cntk_text.txt"), 
+        streamConfigurations, 
+        MinibatchSource.InfinitelyRepeat)
+
+let featureStreamInfo = minibatchSource.StreamInfo(featureStreamName)
+let labelStreamInfo = minibatchSource.StreamInfo(labelsStreamName)
+
+// set per sample learning rate
+let config = {
+    MinibatchSize = 64
+    Epochs = 5
+    Device = DeviceDescriptor.CPUDevice
+    }
+let predictor = learn minibatchSource (featureStreamName,labelsStreamName) config spec
+
+predictor.Save(modelFile)
+
+// validate the model: this still needs a lot of work to look decent
+let minibatchSourceNewModel = 
+    MinibatchSource.TextFormatMinibatchSource(
+        Path.Combine(ImageDataFolder, "Test_cntk_text.txt"), 
+        streamConfigurations, 
+        MinibatchSource.FullDataSweep)
+
+let ValidateModelWithMinibatchSource(
+    modelFile:string, 
+    testMinibatchSource:MinibatchSource,
+    imageDim:int[], 
+    numClasses:int, 
+    featureInputName:string, 
+    labelInputName:string, 
+    device:DeviceDescriptor, 
+    maxCount:int
+    ) =
+
+        let model : Function = Function.Load(modelFile, device)
+        let imageInput = model.Arguments.[0]
+        let labelOutput = model.Output
+
+        let featureStreamInfo = testMinibatchSource.StreamInfo(featureInputName)
+        let labelStreamInfo = testMinibatchSource.StreamInfo(labelInputName)
+
+        let batchSize = 50
+
+        let rec countErrors (total,errors) =
+
+            printfn "Total: %i; Errors: %i" total errors
+
+            let minibatchData = testMinibatchSource.GetNextMinibatch((uint32)batchSize, device)
+
+            if (minibatchData = null || minibatchData.Count = 0)
+            then (total,errors)        
+            else
+
+                let total = total + minibatchData.[featureStreamInfo].numberOfSamples
+
+                // find the index of the largest label value
+                let labelData = minibatchData.[labelStreamInfo].data.GetDenseData<float32>(labelOutput)
+                let expectedLabels = 
+                    labelData 
+                    |> Seq.map (fun l ->                         
+                        let largest = l |> Seq.max
+                        l.IndexOf largest
+                        )
+
+                let inputDataMap = 
+                    [
+                        imageInput, minibatchData.[featureStreamInfo].data
+                    ]
+                    |> dataMap
+
+                let outputDataMap = 
+                    [ 
+                        labelOutput, null 
+                    ] 
+                    |> dataMap
+                    
+                model.Evaluate(inputDataMap, outputDataMap, device)
+
+                let outputData = outputDataMap.[labelOutput].GetDenseData<float32>(labelOutput)
+                let actualLabels =
+                    outputData 
+                    |> Seq.map (fun l ->                         
+                        let largest = l |> Seq.max
+                        l.IndexOf largest
+                        )
+
+                let misMatches = 
+                    (actualLabels,expectedLabels)
+                    ||> Seq.zip
+                    |> Seq.sumBy (fun (a, b) -> if a = b then 0 else 1)
+
+                let errors = errors + misMatches
+
+                if (int total > maxCount)
+                then (total,errors)
+                else countErrors (total,errors)
+
+        countErrors (uint32 0,0)
+
+let total,errors = 
+    ValidateModelWithMinibatchSource(
+        modelFile, 
+        minibatchSourceNewModel,
+        [|imageSize|], 
+        numClasses, 
+        featureStreamName, 
+        labelsStreamName, 
+        DeviceDescriptor.CPUDevice,
+        1000)
+
+printfn "Total: %i / Errors: %i" total errors
