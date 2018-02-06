cdcd327dfa2975514c3d1903223d6f0eceb61064
Mathias Brandewinder <mathias.brandewinder@gmail.com>
2018-02-05 21:37:33 -0800

    Fixed bug in Sequential

--- CNTK.FSharp/Sequential.fs
+++ CNTK.FSharp/Sequential.fs
@@ -288,7 +288,7 @@ module Sequential =
                     // the last minibatch data will be marked as end of a sweep. We use this flag
                     // to count number of epochs.
                     let epoch = 
-                        if isSweepEnd (minibatchData.Values)
+                        if Minibatch.isSweepEnd (minibatchData.Values)
                         then epoch - 1
                         else epoch
 
